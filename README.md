cf89d29360e3a83978ccfb632299adc26b5cfdb7
plasticuproject <nicholes.bowling@gmail.com>
2018-07-23 19:02:47 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
 # clevercord
-Discord bot to chat with Cleverbot
+Discord bot to chat with Cleverbot. Uses the cleverbotfree library to <br />
+communicate via a headless Firefox browser.
+
+## Requirments
+Python 3.x, pip, and the latest version of Firefox browser installed. <br />
+```
+pip install cleverbotfree
+pip install -U https://github.com/Rapptz/discord.py/archive/rewrite.zip
+```
+
+## Installation
+Move the file bot.py to your discord module (discord/ext/commands/) and <br />
+replace the existing file. I hacked up this file so you don't have to surround <br />
+your message text with quotations. <br />
+Example:
+```
+mv bot.py ~/Envs/clevercord/lib/python3.6/site-packages/discord/ext/commands/
+```
+
+<b>Drivers</b>
+
+Selenium requires a driver to interface with the headless browser. Firefox <br />
+requires geckodriver, which needs to be installed before this module can be <br />
+used. Make sure it’s in your PATH, e. g., place it in /usr/bin or /usr/local/bin. <br />
+
+You can download the geckodriver at https://github.com/mozilla/geckodriver/releases <br />
+
+Failure to observe this step will give you the error <br />
+"Message: ‘geckodriver’ executable needs to be in PATH." <br />
+
+<b>Discord</b>
+Head over to https://discordapp.com/developers/applications/me and create a new app. <br />
+Click on "Create Bot User". <br />
+Once done, you can get the secret bot token. <br />
+
+In reasons.py replace all <...> with appropriate information. <br />
+
+To start bot, run:
+```
+python cleverbot.py
+```
+To add bot to server add your CLIENT_ID to this URL and visit in browser:  <br />
+"https://discordapp.com/oauth2/authorize?client_id=<YOUR_BOT_CLIENT_ID_GOES_HERE>&scope=bot" <br />
+
+## Usage
+When bot is active in server, just type "&chat" followed by what you want to send to Cleverbot <br /.
---
+++ cleverbot.py
@@ -0,0 +1,73 @@
+'''
+Discord bot to chat with Cleverbot. Uses the cleverbotfree library to <br />
+communicate via a headless Firefox browser.
+Just message "&chat" to chat with Cleverbot.
+
+Copyright (C) 2018  plasticuproject@pm.me
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+'''
+
+import discord
+from discord.ext import commands
+from cleverbotfree.cbfree import Cleverbot
+
+bot = commands.Bot(command_prefix='&')
+sendCb = Cleverbot().single_exchange
+
+
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+
+
+# uses cleverbotfree to communicate with Cleverbot
+@bot.command()
+async def chat(ctx, userInput):
+    response = sendCb(userInput)
+    await ctx.send(response)
+
+    
+@bot.command()
+async def info(ctx):
+    embed = discord.Embed(title='cleverbot', description='Cleverbot chatbot.',
+                          color=0xeee657)
+    
+    # give info about you here
+    embed.add_field(name='Author', value='<...>')
+    
+    # Shows the number of servers the bot is member of.
+    embed.add_field(name='Server count', value=f'{len(bot.guilds)}')
+
+    # give users a link to invite this bot to their server
+    embed.add_field(name='Invite', 
+     value='https://discordapp.com/oauth2/authorize?client_id=<...>&scope=bot')
+
+    await ctx.send(embed=embed)
+
+bot.remove_command('help')
+
+
+@bot.command()
+async def help(ctx):
+    embed = discord.Embed(title='cleverbot', 
+    description='Cleverbot chatbot. List of commands are:', color=0xeee657)
+    embed.add_field(name='&chat', value='Have a chat with Cleverbot', 
+                    inline=False)
+    embed.add_field(name='&info', value='Gives a little info about the bot', 
+                    inline=False)
+    embed.add_field(name='&help', value='Gives this message', inline=False)
+
+    await ctx.send(embed=embed)
+
+bot.run('<...>')
